760722c7f2748342be86aebbfba6724896f73f4b
Timothy Hsieh <Tim@Cortana.local>
2016-09-19 15:33:45 -0400

    Added subtraction, multiplication, and division functions for objects with the same code.

--- currency_class.rb
+++ currency_class.rb
@@ -1,27 +1,52 @@
+class DifferentCurrencyCodeError < StandardError
+end
+
 class Currency
-# attr_reader :@amount, :@usd, :@eur, :@jpy
-  def initialize(amount, currency_code) #makes a money
+  attr_reader :amount, :currency_code
+
+  def initialize(amount, code)
     @amount = amount
-    @currencycode = currency_code
-
-    puts "Enter amount of currency: "
-    amount = gets.to_i
-    return amount
-
-    puts "Enter 3-letter currency code: "
-    currency_code = gets.downcase
-    return currency_code
-
-    puts "You have #{@amount} #{@currency_code} ready for calculation or conversion."
+    @code = code
   end
 
-  def money(amount, usd, eur, jpy)
-    @amount = amount
-    @usd = usd
-    @eur = eur
-    @jpy = jpy
+  def ==(other)
+    self.amount == other.amount && self.code == other.code
   end
 
-  def select_
+  def !=(other)
+    self.amount != other.amount || self.code != other.code
+  end
+
+  def +(other)
+    if @code == other.code
+      Currency.new(@amount + other.amount, @code)
+    else
+      raise DifferentCurrencyCodeError, "Currency codes do not match!"
+    end
+  end
+
+  def -(other)
+    if @code == other.code
+      Currency.new(@amount - other.amount, @code)
+    else
+      raise DifferentCurrencyCodeError, "Currency codes do not match!"
+    end
+  end
+
+  def *(other)
+    if @code == other.code
+      Currency.new(@amount * other.amount, @code)
+    else
+      raise DifferentCurrencyCodeError, "Currency codes do not match!"
+    end
+  end
+
+  def /(other)
+    if @code == other.code
+      Currency.new(@amount / other.amount, @code)
+    else
+      raise DifferentCurrencyCodeError, "Currency codes do not match!"
+    end
+  end
 
 end
