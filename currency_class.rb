1d4b5f666f1fbbc823d12828eb63cac3b079d060
Timothy Hsieh <Tim@Cortana.local>
2016-09-19 15:29:21 -0400

    Bryce's Example. Added ability to check for same amount/code, ability to check for differing amounts or codes, and ability to add like quantities with same currency codes.

--- currency_class.rb
+++ currency_class.rb
@@ -1,3 +1,27 @@
-class Currency(amount, currencycode)
+class Currency
+# attr_reader :@amount, :@usd, :@eur, :@jpy
+  def initialize(amount, currency_code) #makes a money
+    @amount = amount
+    @currencycode = currency_code
+
+    puts "Enter amount of currency: "
+    amount = gets.to_i
+    return amount
+
+    puts "Enter 3-letter currency code: "
+    currency_code = gets.downcase
+    return currency_code
+
+    puts "You have #{@amount} #{@currency_code} ready for calculation or conversion."
+  end
+
+  def money(amount, usd, eur, jpy)
+    @amount = amount
+    @usd = usd
+    @eur = eur
+    @jpy = jpy
+  end
+
+  def select_
 
 end
